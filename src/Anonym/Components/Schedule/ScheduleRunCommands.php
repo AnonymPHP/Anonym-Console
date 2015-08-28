b31d9ff817c98b45fab71284b4902b5aebbfce28
anonymframework <serif.saglam119@outlook.com>
2015-08-28 23:17:48 +0300

    made some updates

--- src/Anonym/Components/Schedule/ScheduleRunCommands.php
+++ src/Anonym/Components/Schedule/ScheduleRunCommands.php
@@ -14,6 +14,7 @@ namespace Anonym\Components\Console\Schedule;
 
 use Anonym\Components\Console\Command;
 use Anonym\Components\Console\HandleInterface;
+use Anonym\Components\Cron\Cron;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Input\InputInterface;
 
@@ -34,6 +35,20 @@ class ScheduleRunCommands extends Command
      */
     protected $description = 'Run the scheduled commands';
 
+    /**
+     * the instance of cron
+     *
+     * @var Cron
+     */
+    private $schedule;
+
+    /**
+     *
+     */
+    public function __construct()
+    {
+        $this->schedule = new Cron();
+    }
     /**
      * Komut yakalandığı zaman tetiklenecek fonksiyonlardan biridir
      * @return mixed
