31f6d4a5179fba7fbd66bef68b129be6c3672e73
anonymframework <serif.saglam119@outlook.com>
2015-08-30 14:33:32 +0300

    fixed a little bug

--- src/Anonym/Components/GetCommands.php
+++ src/Anonym/Components/GetCommands.php
@@ -1,89 +1,92 @@
 <?php
-    /**
-     * @author vahitserifsaglam <vahit.serif119@gmail.com>
-     * @copyright AnonymMedya, 2015
-     */
+/**
+ * @author vahitserifsaglam <vahit.serif119@gmail.com>
+ * @copyright AnonymMedya, 2015
+ */
 
-    namespace Anonym\Components\Console;
+namespace Anonym\Components\Console;
 
-    use Anonym\Components\Cron\Cron;
-    use Console\System;
+use Anonym\Components\Console\Schedule\ScheduleRunCommands;
+use Anonym\Components\Cron\Cron;
+use Console\System;
+/**
+ * Class CommandsManager
+ * @package Anonym\Console
+ */
+class GetCommands extends System
+{
+
     /**
-     * Class CommandsManager
-     * @package Anonym\Console
+     * the list of default console commands
+     *
+     * @var array
      */
-    class GetCommands extends System
+    private $defaultCommands = [
+        ScheduleRunCommands::class
+    ];
+    /**
+     * @param array $commands
+     */
+    public function __construct(array $commands = [])
     {
-
-        /**
-         * the list of default console commands
-         *
-         * @var array
-         */
-        private $defaultCommands = [];
-        /**
-         * @param array $commands
-         */
-        public function __construct(array $commands = [])
-        {
-            if (count($commands) > 0) {
-                $this->setCommands($commands);
-            }
-
-            if (count($this->defaultCommands)) {
-                $this->addDefaultCommands();
-            }
-
-            $this->resolveSchedule();
-
+        if (count($commands) > 0) {
+            $this->setCommands($commands);
         }
 
-        /**
-         * add default commands to command reposity
-         */
-        private function addDefaultCommands()
-        {
-            foreach($this->defaultCommands as $command)
-            {
-                $this->addCommand($command);
-            }
-        }
-        /**
-         *  resolve the schude
-         */
-        private function resolveSchedule()
-        {
-            if(method_exists($this, 'schedule'))
-            {
-                call_user_func([$this, 'schedule'], new Cron());
-            }
-        }
-        /**
-         * @param array $commands
-         * @return $this
-         */
-        public function setCommands(array $commands)
-        {
-            $this->commands = $commands;
-            return $this;
+        if (count($this->defaultCommands)) {
+            $this->addDefaultCommands();
         }
 
-        /**
-         * Yeni Komut ekler
-         * @param string $command
-         */
-        public function addCommand($command = '')
-        {
-            $this->commands[] = $command;
-        }
-
-        /**
-         * @return array
-         */
-        public function getCommands()
-        {
-
-            return $this->commands;
-        }
+        $this->resolveSchedule();
 
     }
+
+    /**
+     * add default commands to command reposity
+     */
+    private function addDefaultCommands()
+    {
+        foreach($this->defaultCommands as $command)
+        {
+            $this->addCommand($command);
+        }
+    }
+    /**
+     *  resolve the schude
+     */
+    private function resolveSchedule()
+    {
+        if(method_exists($this, 'schedule'))
+        {
+            call_user_func([$this, 'schedule'], new Cron());
+        }
+    }
+    /**
+     * @param array $commands
+     * @return $this
+     */
+    public function setCommands(array $commands)
+    {
+        $this->commands = $commands;
+        return $this;
+    }
+
+    /**
+     * Yeni Komut ekler
+     * @param string $command
+     */
+    public function addCommand($command = '')
+    {
+        $this->commands[] = $command;
+    }
+
+    /**
+     * @return array
+     */
+    public function getCommands()
+    {
+
+        return $this->commands;
+    }
+
+}
